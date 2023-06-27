d146522906382ba6b80fc327cdf9ca305c0903cd
dovakeen118 <kegillis118@gmail.com>
2023-06-27 10:20:45 -0400

    finished code

--- src/routes/magicalCreaturesRouter.js
+++ src/routes/magicalCreaturesRouter.js
@@ -1,43 +1,26 @@
 import express from "express"
 
-// `creatures` variable should return to you the creature objects that you need!
 import creatures from "../creatures.js"
 
 const magicalCreaturesRouter = new express.Router()
 
 magicalCreaturesRouter.get("/", (req, res) => {
-  // 1. define the magical creatures route
-  // 2. BEFORE ANY DATA - is to get a view template rendering on the page with static HTML
-  // debugger
-  const creaturesArray = creatures
-  res.render("magicalCreatures", {
-    creaturesData: creaturesArray,
-    moreStuffs: "hi!"
-  })
-  // in view template, access data via key - creaturesData
+  // console.log("router", creatures)
+  res.render("index", { allCreatures: creatures })
 })
 
-// creatures.forEach((creature, index) => {
-
-// })
-
-// creatures.forEach(creature, index => {
-
-// })
-
 magicalCreaturesRouter.get("/:creatureName", (req, res) => {
-  // which creatureName?
-  // debugger
-  const nameOfCreature = req.params.creatureName
+  // console.log(req.params)
+  const nameFromParams = req.params.creatureName
 
-  let foundCreature
+  let foundCreature = {}
   creatures.forEach((creature) => {
-    // look through `creatures` array to find the creature object whose name matches
-    if (creature.name === nameOfCreature) {
+    if (creature.name === nameFromParams) {
       foundCreature = creature
     }
   })
-
+  // console.log(foundCreature)
+  // res.send("working on show")
   res.render("show", { creature: foundCreature })
 })
 
--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -1,11 +1,13 @@
 import express from "express"
+
 import magicalCreaturesRouter from "./magicalCreaturesRouter.js"
 
 const rootRouter = new express.Router()
 
 rootRouter.get("/", (req, res) => {
   // root route
-  res.render("index")
+  // res.send("hi!")
+  res.render("magical-creatures")
 })
 
 rootRouter.use("/magical-creatures", magicalCreaturesRouter)
