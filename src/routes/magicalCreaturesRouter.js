b2a4b4246e9b046cf78b78e6b6a0ce9dc4b8e773
dovakeen118 <kegillis118@gmail.com>
2023-09-25 15:27:22 -0400

    starter code

--- src/routes/magicalCreaturesRouter.js
+++ src/routes/magicalCreaturesRouter.js
@@ -7,38 +7,8 @@ const magicalCreaturesRouter = new express.Router()
 
 magicalCreaturesRouter.get("/", (req, res) => {
   // 1. define the magical creatures route
-  // 2. BEFORE ANY DATA - is to get a view template rendering on the page with static HTML
+  // 2. BEFORE HANDLING ANY DATA - get a view template rendering on the page with static HTML
   // debugger
-  const creaturesArray = creatures
-  res.render("magicalCreatures", {
-    creaturesData: creaturesArray,
-    moreStuffs: "hi!"
-  })
-  // in view template, access data via key - creaturesData
-})
-
-// creatures.forEach((creature, index) => {
-
-// })
-
-// creatures.forEach(creature, index => {
-
-// })
-
-magicalCreaturesRouter.get("/:creatureName", (req, res) => {
-  // which creatureName?
-  // debugger
-  const nameOfCreature = req.params.creatureName
-
-  let foundCreature
-  creatures.forEach((creature) => {
-    // look through `creatures` array to find the creature object whose name matches
-    if (creature.name === nameOfCreature) {
-      foundCreature = creature
-    }
-  })
-
-  res.render("show", { creature: foundCreature })
 })
 
 export default magicalCreaturesRouter
