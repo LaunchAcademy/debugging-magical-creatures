6271a7e1fb0a6529b625c813380478315350807e
Kerrin Gillis <kerrin.gillis@launchacademy.com>
2023-12-19 09:16:23 -0500

    finished code

--- src/routes/magicalCreaturesRouter.js
+++ src/routes/magicalCreaturesRouter.js
@@ -5,10 +5,33 @@ import creatures from "../creatures.js"
 
 const magicalCreaturesRouter = new express.Router()
 
+// GET localhost:3000/magical-creatures
 magicalCreaturesRouter.get("/", (req, res) => {
+  console.log(req.params)
+  // console.log("magical creatures:", creatures)
   // 1. define the magical creatures route
   // 2. BEFORE HANDLING ANY DATA - get a view template rendering on the page with static HTML
   // debugger
+  res.render("index", { arrayOfCreatures: creatures })
+})
+
+// GET localhost:3000/magical-creatures/dragon
+// GET localhost:3000/magical-creatures/unicorn
+magicalCreaturesRouter.get("/:creatureName", (req, res) => {
+  const nameOfCreatureFromParams = req.params.creatureName
+  console.log(nameOfCreatureFromParams)
+  console.log(req.params)
+
+  // we need to get the specific magical creature object from above params
+  let foundCreature;
+  creatures.forEach((creature) => {
+    if (creature.name === nameOfCreatureFromParams) {
+      foundCreature = creature
+    }
+  })
+  console.log(foundCreature)
+
+  res.render("show", { creatureToShow: foundCreature })
 })
 
 export default magicalCreaturesRouter
--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -3,9 +3,11 @@ import magicalCreaturesRouter from "./magicalCreaturesRouter.js"
 
 const rootRouter = new express.Router()
 
+// localhost:3000/
 rootRouter.get("/", (req, res) => {
   // root route
-  res.render("index")
+  // res.send("hello!")
+  res.render("welcome")
 })
 
 rootRouter.use("/magical-creatures", magicalCreaturesRouter)
